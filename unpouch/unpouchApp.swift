1fe72960ef48de13e643633a2f160252e50b8944
Franciszek Maslanka <ffranekmaslanka@gmail.com>
2026-05-17 21:35:49 +0200

    Initial Commit

---
+++ unpouch/unpouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  unpouchApp.swift
+//  unpouch
+//
+//  Created by dzbanek on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct unpouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
